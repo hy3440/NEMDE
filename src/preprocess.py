968d98ee76ef980c900a21acc754c325311ce693
Yi  Huang <yi.huang@anu.edu.au>
2019-05-03 17:29:06 +1000

    add more downloaders to preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -9,32 +9,41 @@ import zipfile
 # Core data directory
 data_dir = os.path.abspath(os.path.join(os.path.curdir, os.pardir, 'data'))
 
-# Base URL to download data from
+# Base URL to download_files data from
 BASE_URL = "http://nemweb.com.au/Reports/Current"
 
 
-def download(section, filename_pattern):
+def download(url):
+    """
+    Unzip and download file from URL.
+
+    :param url: URL to download file from
+    :return: name of unzipped and downloaded file
+    """
+    result = requests.get(url)
+    if result.ok:
+        with zipfile.ZipFile(io.BytesIO(result.content)) as zf:
+            zf.extractall(data_dir)
+            return zf.namelist()[0]
+
+
+def download_files(section, filename_pattern):
     """
     Download all the matched files from the section.
 
     e.g. Download the files from http://nemweb.com.au/Reports/Current/Bidmove_Summary/ whose name matches the pattern
          PUBLIC_BIDMOVE_SUMMARY_<#CASE_DATE>_[0-9]{16}.zip
 
-    :param section: the section to download from
+    :param section: the section to download_files from
     :param filename_pattern:
     :return: a list of downloaded csv filenames
     """
     page = requests.get("{0}/{1}".format(BASE_URL, section))
     regex = re.compile("{0}<".format(filename_pattern))
-    # match = regex.finditer(page.text)[0]:
     filenames = []
     for match in regex.findall(page.text):
-        result = requests.get("{0}/{1}/{2}".format(BASE_URL, section, match[:-1]))
-        if result.ok:
-            with zipfile.ZipFile(io.BytesIO(result.content)) as zf:
-                csv_name = zf.namelist()[0]
-                filenames.append(csv_name)
-                zf.extractall(data_dir)
+        csv_name = download("{0}/{1}/{2}".format(BASE_URL, section, match[:-1]))
+        filenames.append(csv_name)
     print("{0}: {1} file(s).".format(section, len(filenames)))
     return filenames
 
@@ -52,7 +61,7 @@ def download_bidmove_summary(case_date):
     visibility_id = "PUBLIC"
     file_id = "BIDMOVE_SUMMARY"
     filename_pattern = "{0}_{1}_{2}_[0-9]{{16}}.zip".format(visibility_id, file_id, case_date)
-    return download(section, filename_pattern)
+    return download_files(section, filename_pattern)
 
 
 def download_5min_predispatch(case_date):
@@ -68,7 +77,7 @@ def download_5min_predispatch(case_date):
     visibility_id = "PUBLIC"
     file_id = "P5MIN"
     filename_pattern = "{0}_{1}_{2}[0-9]{{4}}_[0-9]{{14}}.zip".format(visibility_id, file_id, case_date)
-    return download(section, filename_pattern)
+    return download_files(section, filename_pattern)
 
 
 def download_predispatch(case_date):
@@ -84,7 +93,7 @@ def download_predispatch(case_date):
     visibility_id = "PUBLIC"
     file_id = "PREDISPATCHIS"
     filename_pattern = "{0}_{1}_{2}[0-9]{{4}}_[0-9]{{14}}.zip".format(visibility_id, file_id, case_date)
-    return download(section, filename_pattern)
+    return download_files(section, filename_pattern)
 
 
 def download_network_outage(case_date):
@@ -100,7 +109,7 @@ def download_network_outage(case_date):
     visibility_id = "PUBLIC"
     file_id = "NETWORK"
     filename_pattern = "{0}_{1}_{2}[0-9]{{6}}_[0-9]{{16}}.zip".format(visibility_id, file_id, case_date)
-    return download(section, filename_pattern)
+    return download_files(section, filename_pattern)
 
 
 def download_dispatch_summary(case_date):
@@ -116,7 +125,7 @@ def download_dispatch_summary(case_date):
     visibility_id = "PUBLIC"
     file_id = "DISPATCHIS"
     filename_pattern = "{0}_{1}_{2}[0-9]{{4}}_[0-9]{{16}}.zip".format(visibility_id, file_id, case_date)
-    return download(section, filename_pattern)
+    return download_files(section, filename_pattern)
 
 
 def download_dispatch_scada(case_date):
@@ -132,7 +141,27 @@ def download_dispatch_scada(case_date):
     visibility_id = "PUBLIC"
     file_id = "DISPATCHSCADA"
     filename_pattern = "{0}_{1}_{2}[0-9]{{4}}_[0-9]{{16}}.zip".format(visibility_id, file_id, case_date)
-    return download(section, filename_pattern)
+    return download_files(section, filename_pattern)
+
+
+def download_altlimits():
+    """
+    Download the complete list of ratings used in AEMO's EMS (energy management system)
+
+    :return: path of the downloaded file
+    """
+    print("Ratings in EMS: 1 file(s)")
+    return download("http://nemweb.com.au/Reports/Current/Alt_Limits/altlimits.zip")
+
+
+def download_transmission_equipment_ratings():
+    """
+    Download the daily transmission equipment ratings used in constraint equations and the ID used in the right-hand of the constraint equations
+
+    :return: name of the downloaded file
+    """
+    print("Daily transmission equipment ratings: 1 file(s)")
+    return download("http://nemweb.com.au/Reports/Current/Alt_Limits/PUBLIC_TER_DAILY.zip")
 
 
 def download_all(case_date):
@@ -140,7 +169,7 @@ def download_all(case_date):
     Download files of the given date from all sections.
 
     :param case_date: the date of downloaded data
-    :return: a list of downloaded csv filenames
+    :return: None
     """
     download_bidmove_summary(case_date)
     download_5min_predispatch(case_date)
@@ -148,12 +177,13 @@ def download_all(case_date):
     download_network_outage(case_date)
     download_dispatch_summary(case_date)
     download_dispatch_scada(case_date)
+    download_altlimits()
+    download_transmission_equipment_ratings()
 
 
 def main():
     case_date = "20190502"
     print("Download data of {0}".format(case_date))
-    download_all(case_date)
 
 
 if __name__ == '__main__':
