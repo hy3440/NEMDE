30494229921376a83e41f84f9a87695fe13ae666
Yi  Huang <yi.huang@anu.edu.au>
2019-05-06 16:02:14 +1000

    update environment setting

--- export_environment.sh
+++ export_environment.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-conda activate nemde
 rm environment.yml
 conda env export > environment.yml
--- update_environment.sh
+++ update_environment.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+onedrive
 conda activate nemde
 conda env update -f environment.yml
